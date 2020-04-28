dfab9355ab3341e2e2b6e2cd9a1a36c7e8635d19
Francois-418 <francois.marion4@gmail.com>
2020-04-28 17:11:08 +0200

    Monday Update

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,8 +10,11 @@ import { products } from '../products';
 export class ProductListComponent {
   products = products;
 
-  share() {
-    window.alert('The product has been shared!');
+  share(name) {
+    window.alert('The product '+name+' has been shared!');
+  }
+  onNotify(name) {
+    window.alert('You will be notified when the '+name+' goes on sale')
   }
 }
 
